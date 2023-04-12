887f3f6e37188182405b5bc41941ace8b455c9ac
aldemylla <aldemylla@gmail.com>
2023-04-12 14:29:50 -0300

    feat: Added cors library and accepted all origins to API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 
 const routes = require("./routes/ProductRouter");
 const connectToDatabase = require("./database");
@@ -11,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(PORT, () => {
