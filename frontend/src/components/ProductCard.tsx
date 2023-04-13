e1724ecc68f0603ce649eb95f095ba6e8dcc7b96
aldemylla <aldemylla@gmail.com>
2023-04-13 11:48:35 -0300

    feat:Product list stylization

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,11 +1,28 @@
+import { MouseEventHandler, MouseEvent } from "react";
+
 import { useProductsContext } from "@/contexts/ProductsContext";
 import { Product } from "@/types/Product";
-import { MouseEventHandler, MouseEvent } from "react";
+
+import styles from "@/styles/product_card.module.scss";
+import Image from "next/image";
 
 interface ProductCardProps {
   product: Product;
   onClick: MouseEventHandler;
   detailed: boolean;
+}
+
+interface ProductInfoProps {
+  title: string;
+  info: string;
+}
+
+function ProductInfo({ title, info }: ProductInfoProps) {
+  return (
+    <p>
+      <strong>{title}:</strong> {info}
+    </p>
+  );
 }
 
 export function ProductCard({ product, onClick, detailed }: ProductCardProps) {
@@ -24,21 +41,37 @@ export function ProductCard({ product, onClick, detailed }: ProductCardProps) {
   }
 
   return (
-    <div onClick={onClick}>
-      <h2>Código: {codigo}</h2>
-      <p>Descrição: {descricao}</p>
-      {detailed && (
-        <>
-          <p>Preço: {preco}</p>
-          <p>Data do cadastro: {String(data_cadastro)}</p>
-        </>
-      )}
-      <div role='group' aria-label='Ações'>
-        <button type='button' onClick={handleEditProduct}>
-          Editar
+    <div onClick={onClick} className={styles.container}>
+      <div role='group' className={styles.infos_container}>
+        <ProductInfo title={"Código"} info={codigo} />
+        <ProductInfo title={"Descrição"} info={descricao} />
+        {detailed && (
+          <>
+            <ProductInfo title={"Preço"} info={`${preco}`} />
+            <ProductInfo title={"Data do cadastro"} info={`${data_cadastro}`} />
+          </>
+        )}
+      </div>
+      <div role='group' aria-label='Ações' className={styles.actions_container}>
+        <button type='button' onClick={handleEditProduct} className={styles.action_edit}>
+          <Image
+            className={styles.action_icon}
+            src={"/icons/edit.svg"}
+            alt='Editar'
+            width={15}
+            height={15}
+          />
+          <p>Editar</p>
         </button>
-        <button type='button' onClick={handleDeleteProduct}>
-          Deletar
+        <button type='button' onClick={handleDeleteProduct} className={styles.action_delete}>
+          <Image
+            className={styles.action_icon}
+            src={"/icons/delete.svg"}
+            alt='Deletar'
+            width={15}
+            height={15}
+          />
+          <p>Deletar</p>
         </button>
       </div>
     </div>
